f3389fa24b579726413b95c6def63297e399d5f7
masaya <hope.is.a.waking.dream.8.1@gmail.com>
2020-08-11 12:18:25 +0900

    create Movie model

--- db/migrate/20200811031128_create_movies.rb
+++ db/migrate/20200811031128_create_movies.rb
@@ -1,7 +1,9 @@
 class CreateMovies < ActiveRecord::Migration[6.0]
   def change
     create_table :movies do |t|
-
+      t.title :string
+      t.url :string
+      t.content :text
       t.timestamps
     end
   end
