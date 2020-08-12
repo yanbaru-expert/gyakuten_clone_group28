379a791105336bb076ea5dd6e1fc7eef100cb168
masaya <hope.is.a.waking.dream.8.1@gmail.com>
2020-08-13 00:49:33 +0900

    modify

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,7 @@ Bundler.require(*Rails.groups)
 module GyakutenCloneGroup
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 6.0
     # 表示TimeZone
     config.time_zone = 'Tokyo'
 
