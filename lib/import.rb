9b7c178cc533aa599d8abd6b3bf26f3b00d64c23
yuta kimura <rubystudy0123@gmail.com>
2020-08-12 16:46:55 +0900

    import_csv.rakeファイルにてAwsTextデータをインポートできるように修正

--- lib/import.rb
+++ lib/import.rb
@@ -3,28 +3,11 @@ require "csv"
 
 class Import
   def self.csv_data(path:)
+    # CSVファイルの（１行目をキーとして）各行のデータを配列に格納する
+    # CSVファイルのカラム名と，テーブルのカラム名を一致させておく必要がある
+    # row は CSV::Row クラスである。これはモデルの引数として渡せないのでハッシュに変換しておく。
     list = []
-    # CSVファイルからインポートするデータを取得し配列に格納
-    CSV.foreach(path, headers: true) do |row|
-      list << {
-          title: row["title"],
-          content: row["content"]
-      }
-    end
-    puts "インポート処理を開始"
-
-    begin
-      AwsText.transaction do
-        # 例外が発生する可能性のある処理
-        AwsText.create!(list)
-      end
-      # 正常に動作した場合の処理
-      puts "インポート完了!!"
-    # 例外処理
-    rescue ActiveModel::UnknownAttributeError => invalid
-      # 例外が発生した場合の処理
-      # インポートができなかった場合の例外処理
-      puts "インポートに失敗：UnknownAttributeError"
-    end
+    CSV.foreach(path, headers: true) { |row| list << row.to_h }
+    list
   end
 end
--- lib/tasks/import_csv.rake
+++ lib/tasks/import_csv.rake
@@ -5,7 +5,21 @@ namespace :import_csv do
   desc "AwsTextデータのインポート"
 
   task AwsText: :environment do
-    Import.csv_data(path: 'db/csv_data/aws_text_data.csv')
+    list = Import.csv_data(path: 'db/csv_data/aws_text_data.csv')
+    puts "インポート処理を開始"
+    begin
+      AwsText.transaction do
+        # 例外が発生する可能性のある処理
+        AwsText.create!(list)
+      end
+      # 正常に動作した場合の処理
+      puts "インポート完了!!"
+    # 例外処理
+    rescue ActiveModel::UnknownAttributeError => invalid
+      # 例外が発生した場合の処理
+      # インポートができなかった場合の例外処理
+      puts "インポートに失敗：UnknownAttributeError"
+    end
   end
 
 end
