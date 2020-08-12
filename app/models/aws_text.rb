2272759c8f571649a7ad31ed99ab103804548f37
zoguxy <bsaa.stars7661@gmail.com>
2020-08-12 21:56:18 +0900

    10_バリデーションを追加

--- app/models/aws_text.rb
+++ app/models/aws_text.rb
@@ -1,2 +1,4 @@
 class AwsText < ApplicationRecord
+  validates :title, presence: true, length: { maximum: 50 }
+  validates :contents, presence: true, length: { maximum: 500 }
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,5 @@
 class Movie < ApplicationRecord
+  validates :title, presence: true, length: { maximum: 50 }
+  validates :url, presence: true, length: { maximum: 300 }
+  validates :contents, presence: true, length: { maximum: 500 }
 end
