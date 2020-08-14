c58d88e7f94d68195d99e848e8d46c09ea7622a0
masaya <hope.is.a.waking.dream.8.1@gmail.com>
2020-08-14 10:44:01 +0900

    add active_admin

--- Gemfile
+++ Gemfile
@@ -23,6 +23,9 @@ gem 'devise'
 gem 'rails-i18n', '~> 6.0.0'
 gem 'devise-i18n'
 
+#管理者画面
+gem 'activeadmin'
+
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@ PASSWORD = 'password'
 User.find_or_create_by!(email: EMAIL) do |user|
   user.password = PASSWORD
   puts 'ユーザーの初期データインポートに成功しました。'
-end
+endAdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
