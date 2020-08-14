3db703390046b68d48d7a93032666df8ecbcaa7f
masaya <hope.is.a.waking.dream.8.1@gmail.com>
2020-08-14 23:09:53 +0900

    add active_admin:resource

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,4 +15,3 @@ AdminUser.find_or_create_by!(email: EMAIL) do |user|
   user.password = PASSWORD
   puts '管理者認識しました。'
 end
-#AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
