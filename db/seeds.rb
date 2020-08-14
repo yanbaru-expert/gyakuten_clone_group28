c246c1e2fec428e362d984c72e201002607fd64e
masaya <hope.is.a.waking.dream.8.1@gmail.com>
2020-08-14 20:17:37 +0900

    create admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,11 @@ PASSWORD = 'password'
 User.find_or_create_by!(email: EMAIL) do |user|
   user.password = PASSWORD
   puts 'ユーザーの初期データインポートに成功しました。'
-endAdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+end
+
+EMAIL = 'admin@example.com'
+PASSWORD = 'password'
+AdminUser.find_or_create_by!(email: EMAIL) do |user|
+  user.password = PASSWORD
+  puts 'ユーザーを管理者として認識しました。'
+end
