be4f55001df5a4f96d109c6f94fefe25b056d55b
masaya <hope.is.a.waking.dream.8.1@gmail.com>
2020-08-28 21:47:19 +0900

    fix watched_movie

--- app/controllers/watched_movies_controller.rb
+++ app/controllers/watched_movies_controller.rb
@@ -1,11 +1,11 @@
 class Watched_MoviesController< ApplicationController
     def create
-      current_user.watched_movies.create!(id: params[:id])
+      current_user.watched_movies.create!(movie_id: params[:movie_id])
       redirect_to root_path
     end
 
     def destroy
-      current_user.watched_movies.find_by(id: params[:id]).destroy!
+      current_user.watched_movies.find_by(movie_id: params[:movie_id]).destroy!
       redirect_to root_path
     end
 end
