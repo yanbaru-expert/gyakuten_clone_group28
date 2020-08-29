3ac1a9aa2b4955e050103ba81339f485b243ba57
masaya <hope.is.a.waking.dream.8.1@gmail.com>
2020-08-30 00:21:31 +0900

    modify watched

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,5 +2,7 @@ class MoviesController < ApplicationController
   PER_PAGE = 18
   def index
     @movies = Movie.order(id: :asc).page(params[:page]).per(PER_PAGE)
+    @movies = Movie.includes(:user)
+    @learnd_movie_ids = current_user.watched_movies.pluck(:movie_id)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   has_many :movies, dependent: :destroy
   has_many :watched_movies, dependent: :destroy
+  # ***** 以下を追加 *****
+  # user.liked_messages で user が「いいね!」しているメッセージ一覧が取得できるようになる
+  has_many :learned_movies, through: :watched_movies, source: :movie
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
